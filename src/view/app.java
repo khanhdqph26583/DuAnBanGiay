489cd8920305316bcb8422d8a697fa6f1459f178
MSI\AS <linhltmph26606@fpt.edu.vn>
2022-11-28 09:01:44 +0700

    no message

---
+++ src/view/app.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author AS
+ */
+public class app {
+    public static void main(String[] args) {
+        new FrmKhuyenMaiView().setVisible(true);
+    }
+    
+}
